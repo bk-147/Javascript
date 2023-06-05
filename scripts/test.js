b71244239cfb332b22cdf78b0ea4a55242d50f4d
Bhaarath Karthikeyan <56688291+bk-147@users.noreply.github.com>
2023-06-05 11:07:12 +0530

    Added test.js

---
+++ scripts/test.js
@@ -0,0 +1,8 @@
+console.log("hello world")
+let x = "heads";
+const guess = prompt("heads/ tails ? ");
+if (x === guess) {
+    alert("congrats, coin landed in heads");
+} else {
+    alert("awww, coin landed in tails");
+}
