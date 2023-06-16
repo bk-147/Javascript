55e33290487100bfe665b3793a4b4d0d18a271a3
Bhaarath Karthikeyan <56688291+bk-147@users.noreply.github.com>
2023-06-17 01:39:35 +0530

    added mongo_node

---
+++ mongo_node/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const movies = require('./movies')
+
+
+
+app.use(express.json());
+app.use('/api/v1/movies', movies);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
